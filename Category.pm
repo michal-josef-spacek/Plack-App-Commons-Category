c11c02a25296c7c2be8232a116e5e0782eef01ca
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-07-06 19:14:44 +0200

    Add CSS for Tags::HTML::Form

--- Category.pm
+++ Category.pm
@@ -36,6 +36,8 @@ sub _css {
 	if ($self->{'_page'} eq 'category') {
 		$self->{'_html_image_grid'}->process_css;
 		$self->{'_html_pager'}->process_css;
+	} elsif ($self->{'_page'} eq 'category_form') {
+		$self->{'_html_form'}->process_css;
 	} elsif ($self->{'_page'} eq 'image') {
 		$self->{'_html_image'}->process_css;
 	}
